77d7bef03e28d8e374c3a81c7ab3978484691f7a
Itai <itai.avis@gmail.com>
2024-11-24 00:10:18 +0200

    WIP. Initial menu, and the first two options.

--- ex2.c
+++ ex2.c
@@ -7,23 +7,78 @@ Assignment: ex2
 #include <stdio.h>
 
 int main() {
-	// Case 1: Draw Happy Face with given symbols for eyes, nose and mouse
-	/* Example:
-	* n = 3:
-	* 0   0
-	*   o
-	* \___/
-	*/
-    
-
-	// Case 2: determine whether the sum of all digits to the left of the middle digit(s)
-	// and the sum of all digits to the right of the middle digit(s) are equal
-	/* Examples:
-	Balanced: 1533, 450810, 99
-	Not blanced: 1552, 34
-	Please notice: the number has to be bigger than 0.
-	*/
-
+	int menuOption, faceSize = 0, digitNumber, digitSumL = 0, digitSumR = 0, digitCount = 0, digitTempnum;
+	char faceEyes, faceNose, faceMouth;
+	while (menuOption != 7) {
+		printf("Choose an option:\n\t1. Happy Face\n\t2. Balanced Number\n\t3. Generous Number\n\t4. Circle Of Joy\n\t5. Happy Numbers\n\t6. Festival of Laughter\n\t7. Exit\n");
+		scanf("%d", &menuOption);
+		if (menuOption == 7) {
+			printf("Thank you for your journey through Numeria!\n");
+				break;
+		}
+		if ((menuOption > 7) || (menuOption < 1)) {
+			printf("This option is not available, please try again.\n");
+			continue;
+		}
+		// Case 1: Draw Happy Face with given symbols for eyes, nose and mouse and size
+		if (menuOption == 1) {
+			printf("Enter symbols for the eyes, nose, and mouth:\n");
+			scanf("%c %c %c", &faceEyes, &faceNose, &faceMouth);
+			printf("Enter face size:\n");
+			scanf("%d", &faceSize);
+			while ((faceSize < 0) || (faceSize % 2 == 0)) {
+				printf("The face's size must be an odd and positive number, please try again:\n");
+				scanf("%d", &faceSize);
+			}
+			printf("\n%c", faceEyes);
+			for (int i = 0; i < faceSize; i++) {
+			printf(" ");
+			}
+			printf("%c\n", faceEyes);
+			for (int i = 0; i < faceSize / 2; i++) {
+				printf(" ");
+			}
+			printf("%c", faceNose);
+			for (int i = 0; i < faceSize / 2; i++) {
+				printf(" \n");
+			}
+			printf("\\");
+			for (int i = 0; i < faceSize; i++) {
+				printf("%c", faceMouth);
+			}
+			printf("/\n");
+		}
+		// Case 2: determine whether the sum of all digits to the left of the middle digit(s) and the sum of all digits to the right of the middle digit(s) are equal
+		if (menuOption == 2) {
+			printf("Enter a number:\n");
+			do{
+				scanf("%d", &digitNumber);
+				if (digitNumber < 1) {
+					printf("Only positive number is allowed, please try again:\n");
+				}
+			} while (digitNumber < 1);
+			digitTempnum = digitNumber;
+			while (digitTempnum != 0) {
+				digitTempnum /= 10;
+				digitCount++;
+			}
+			digitTempnum = digitNumber;
+			for (int i = 0; i < digitCount / 2; i++) {
+				digitSumL += digitTempnum % 10;
+				digitTempnum /= 10;
+			}
+			digitTempnum /= 10;
+			for (int i = 0; i < digitCount / 2; i++) {
+				digitSumR += digitTempnum % 10;
+				digitTempnum /= 10;
+			}
+			if (digitSumL == digitSumR) {
+				printf("Balanced\n");
+			}
+			else {
+				printf("Not Balanced\n");
+			}
+		}
 	// Case 3: determine whether the sum of the proper divisors (od an integer) is greater than the number itself
 	/* Examples:
 	Abudant: 12, 20, 24
@@ -55,4 +110,5 @@ int main() {
 	6, smile: 2, cheer: 3 : 1, Smile!, Cheer!, Smile!, 5, Festival!
 	*/
 	return 0;
+	}
 }
